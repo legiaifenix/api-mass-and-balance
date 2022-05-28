6338f2900798622a640cac5bfe68e5ec7c983b80
Diogo Santo <diogo.santo@raisin.co.uk>
2022-05-28 17:27:32 +0100

    Adding check for body

--- cypress/integration/1-routes/routes.spec.js
+++ cypress/integration/1-routes/routes.spec.js
@@ -3,6 +3,7 @@ describe('Testing api routes are accessible', () => {
     it('should be able to visit home page', function () {
         cy.request('GET', Cypress.env('baseURL')).then((response) => {
             expect(response.status).to.eq(200);
+            expect(response.body).to.eq('<h1>Mass and Balance</h1>');
         });
     });
 
